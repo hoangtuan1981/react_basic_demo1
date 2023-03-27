58ad30718e6c6c90a4bbd440e76fe5cbe9cc515d
hoangleanhtuan1981 <hoangleanhtuan1981@hotmail.com>
2023-03-27 17:52:36 +0700

    added menu. Use react route.

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,50 @@
 //import logo from './logo.svg';
+//import * as ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes, Route
+} from "react-router-dom";
+import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
 import './App.css';
+import Nav from './Nav/Nav'
 import ToDoList from './ToDo/ToDoList';
 //import AddToDo from './AddToDo';
+import About from './About';
+
+/*
 function App() {
   return (
     <div className="App">
       <p>
           Learning basic React
         </p>
-
-      <ToDoList/>
     </div>
   );
+}
+*/
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+      <Nav />
+        <p>
+          Learning basic React
+        </p>
+        <Router>
+          <Routes>
+            <Route path="" />
+            <Route path="/todo" element={<ToDoList />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+
+      </header>
+
+    </div>
+
+  )
 }
 
 export default App;
