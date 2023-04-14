ff581abb1b6589b48acb413dd46c757cbc7cc78a
hoangleanhtuan1981 <hoangleanhtuan1981@hotmail.com>
2023-04-14 11:17:02 +0700

    Enable to see redux in schrome redux dev tool.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux'
 import { createStore  } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 
-const store = createStore (rootReducer)
+const store = createStore (rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  )
 
 ReactDOM.render(
   <Provider store={store}>
